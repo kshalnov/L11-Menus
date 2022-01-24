7f0570b26cb739670144eb6676f9ccde343cbc05
Kirill Shalnov <borhammere@gmail.com>
2022-01-24 20:45:36 +0300

    Add xml menu

--- app/src/main/java/ru/gb/course1/l11_menus/MainActivity.java
+++ app/src/main/java/ru/gb/course1/l11_menus/MainActivity.java
@@ -1,14 +1,53 @@
 package ru.gb.course1.l11_menus;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "@@@";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(v -> {
+            Toast.makeText(this, "setOnClickListener", Toast.LENGTH_SHORT).show();
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu() called with: menu = [" + menu + "]");
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected() called with: item = [" + item + "]");
+        switch (item.getItemId()) {
+            case R.id.menu_about:
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_add:
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_refresh:
+                Toast.makeText(this, item.getTitle() + " onOptionsItemSelected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_settings:
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
